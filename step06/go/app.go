3f883cf385965600ff5e7c3c5ded04fbc46ec9b5
高山沙也加 <sayaka@takayamasayaka-no-MacBook-Pro.local>
2017-07-03 21:27:36 +0900

    string+string

--- step06/go/app.go
+++ step06/go/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func init() {
@@ -11,5 +12,16 @@ func init() {
 
 func handlePata(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "Hello world!\n")
+
+	s_a_0:=strings.Split(r.Form["a"][0], "")
+//	s_a_1:=strings.Split(r.Form["a"][1], "")	
+	s_b_0:=strings.Split(r.Form["b"][0], "")
+//	s_b_1:=strings.Split(r.Form["b"][1], "")
+	
+	var ans1 string	
+	for i:=0;i<len(s_a_0);i++{
+	    ans1+=ans1+s_a_0[i]+s_b_0[i]
+}
+fmt.Fprintf(w,"Hello,world")
+	//fmt.Fprintf(w,ans1)
 }
